2a2fb9859b1267c18aba8aaebe4b11ca9cc12ef2
Justin Vega <justin.vega27@gmail.com>
2020-05-27 08:18:59 -0400

    initial project creation

---
+++ SwiftApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftApp
+//
+//  Created by Justin Vega on 5/27/20.
+//  Copyright © 2020 Justin Vega. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
